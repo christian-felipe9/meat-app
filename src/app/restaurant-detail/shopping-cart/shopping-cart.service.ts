5b73b00401048fb483211accec6a7094be64e2f3
Christian Felipe Lopes Dias <Christian_Felipe9@hotmail.com>
2019-04-02 22:04:28 -0300

    Order page completed

--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -9,13 +9,27 @@ export class ShoppingCartService{
   }
 
   addItem(item: MenuItem){
-    let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id)
+    let foundItem = this.items.find((mItem) => mItem.menuItem.id === item.id);
 
-    if(foundItem){
-    foundItem.quantity = foundItem.quantity + 1;
+    if(foundItem)
+    {
+      this.increaseQty(foundItem);
     }
-    else{
-    this.items.push(new CartItem(item));
+
+    else
+    {
+      this.items.push(new CartItem(item));
+    }
+  }
+
+  increaseQty(item: CartItem){
+    item.quantity = item.quantity + 1;
+  }
+
+  decreaseQty(item: CartItem){
+    item.quantity = item.quantity - 1;
+    if(item.quantity === 0){
+      this.removeItem(item);
     }
   }
 
