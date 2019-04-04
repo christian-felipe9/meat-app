0b4f14ce15b916d8e3b7dc84118cc7c031507deb
Christian Felipe Lopes Dias <Christian_Felipe9@hotmail.com>
2019-04-04 20:39:38 -0300

    Rating component completed and animated

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mt-rating',
   templateUrl: './rating.component.html'
 })
 export class RatingComponent implements OnInit {
+
+  @Output() rated = new EventEmitter<number>();
 
   rates: number[] = [1, 2, 3, 4, 5];
 
@@ -19,6 +21,22 @@ export class RatingComponent implements OnInit {
 
   setRate(r: number){
   this.rate = r;
+  this.previousRate = undefined;
+  this.rated.emit(this.rate);
+  }
+
+  setTemporaryRate(r: number){
+    if(this.previousRate === undefined){
+      this.previousRate = this.rate;
+    }
+    this.rate = r;
+  }
+
+  clearTemporaryRate(){
+    if(this.previousRate !== undefined){
+      this.rate = this.previousRate;
+    }
+    this.previousRate = undefined;
   }
 
 }
