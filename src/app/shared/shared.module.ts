4789267aad43b121615d29b5b9b533615c074d35
Christian Felipe Lopes Dias <Christian_Felipe9@hotmail.com>
2019-04-16 21:35:52 -0300

    Shared Module created and Lazy and Pre Loading implemented

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+import { InputComponent } from "./input/input.component";
+import { RadioComponent } from "./radio/radio.component";
+import { RatingComponent } from "./rating/rating.component";
+
+import { OrderService } from "app/order/order.service";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+
+@NgModule({
+  declarations: [InputComponent, RadioComponent, RatingComponent],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  exports: [InputComponent, RadioComponent, RatingComponent,
+            FormsModule, ReactiveFormsModule, CommonModule]
+})
+export class SharedModule{
+  static forRoot(): ModuleWithProviders{
+    return{
+      ngModule: SharedModule,
+      providers: [ShoppingCartService, RestaurantsService, OrderService]
+    }
+  }
+}
